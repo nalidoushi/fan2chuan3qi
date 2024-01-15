cbf9549375f1e6bac998f0f3cefd62e88221dab1
范传奇 <83123095@qq.com>
2024-01-15 16:15:55 +0800

    创建TrimDemo类，演示String的trim方法，去除字符串两侧的空白字符

---
+++ src/main/java/string/TrimDemo.java
@@ -0,0 +1,14 @@
+package string;
+
+/**
+ * String trim()
+ * 去除字符串两侧的空白字符
+ */
+public class TrimDemo {
+    public static void main(String[] args) {
+        String str = "   hello          ";
+        System.out.println(str);
+        String trim = str.trim();
+        System.out.println(trim);
+    }
+}
