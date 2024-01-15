f82895d9162ee661ac7a6107cf4330c21bf0a9ca
范传奇 <83123095@qq.com>
2024-01-15 14:53:25 +0800

    创建LengthDemo类，演示String的length方法，获取字符串长度

---
+++ src/main/java/string/LengthDemo.java
@@ -0,0 +1,13 @@
+package string;
+
+/**
+ * int length()
+ * 返回当前字符串的长度(字符的个数)
+ */
+public class LengthDemo {
+    public static void main(String[] args) {
+        String s = "我爱java!";
+        int len = s.length();
+        System.out.println("len:"+len);
+    }
+}
