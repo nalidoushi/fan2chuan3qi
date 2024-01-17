5646e837cdc46ddd868a42daab6cec4bf20745ad
范传奇 <83123095@qq.com>
2024-01-17 12:00:18 +0800

    更新test包和Demo与Demo2,练习画内存图

--- src/main/java/collection/CollectionDemo4.java
+++ src/main/java/collection/CollectionDemo4.java
@@ -17,14 +17,26 @@ public class CollectionDemo4 {
 //        c.add("one");//不是Point类型的都不能存入集合
 //        c.add(123);//不行
         Point p = new Point(1,2);
-        c.add(p);
-        System.out.println("p:"+p);
-        System.out.println("c:"+c);
+        c.add(p);//将p变量的值存入集合,因此集合第一个元素也是一个地址
+        System.out.println("p:"+p);//(1,2)
+        System.out.println("c:"+c);//[(1,2)]
 
         p.setX(2);
 
-        System.out.println("p:"+p);
-        System.out.println("c:"+c);
+        System.out.println("p:"+p);//(2,2)
+        System.out.println("c:"+c);//[(2,2)]  集合内容也发生了变化
+
+        doSome(p,c);
+        System.out.println("p:"+p);//?
+        System.out.println("c:"+c);//?
+    }
+
+    public static void doSome(Point p,Collection c){
+        p.setX(5);
+        p = new Point(6,7);
+        c = new ArrayList();
+        c.add(p);
+        p.setX(8);
     }
 }
 
