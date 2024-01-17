f2bc4e54b30886379dad31478be030085a21c09c
范传奇 <83123095@qq.com>
2024-01-17 10:29:09 +0800

    更新Point类和CollectionDemo2类,演示元素的toString方法和equals方法对集合的影响

--- src/main/java/collection/CollectionDemo2.java
+++ src/main/java/collection/CollectionDemo2.java
@@ -2,6 +2,7 @@ package collection;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 
 /**
  * 元素equals方法与toString方法对集合的影响
@@ -9,17 +10,40 @@ import java.util.Collection;
 public class CollectionDemo2 {
     public static void main(String[] args) {
         Collection c = new ArrayList();
+//        Collection c = new HashSet();
         c.add(new Point(1,2));
         c.add(new Point(3,4));
         c.add(new Point(5,6));
         c.add(new Point(7,8));
         c.add(new Point(9,10));
+        c.add(new Point(1,2));//重复元素也是依靠元素equals比较的
         /*
             集合重写了toString方法,用于展示集合内容
             格式:
             [元素1.toString(), 元素2.toString(), ....]
          */
         System.out.println(c);
+
+        Point p = new Point(1,2);
+        /*
+            boolean contains(Object o)
+            判断当前集合是否包含给定元素
+            判断依据:该元素是否存在与集合现有元素equals比较为true的情况,存在则认为
+            包含该元素.因此元素自身的equals方法决定着判断包含的结果.
+         */
+        boolean contains = c.contains(p);
+        System.out.println("是否包含:"+contains);
+
+        /*
+            boolean remove(Object o)
+            将给定元素从集合中删除.
+            删除的是与给定元素equals比较为true的元素
+            对于List而言，如果是重复元素，仅删除靠前的第一个元素，并非把所有重复的该元素都删除
+         */
+        c.remove(p);
+        System.out.println(c);
+
+
     }
 }
 
--- src/main/java/collection/Point.java
+++ src/main/java/collection/Point.java
@@ -1,5 +1,7 @@
 package collection;
 
+import java.util.Objects;
+
 /**
  * 使用当前类实例测试集合的操作
  */
@@ -38,6 +40,19 @@ public class Point {
                 "x=" + x +
                 ", y=" + y +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return x == point.x && y == point.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
     }
 }
 
