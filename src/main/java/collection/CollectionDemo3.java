7aa8e5366d5d680c533d6cba0d9c86c6fb39bb1c
范传奇 <83123095@qq.com>
2024-01-17 10:54:14 +0800

    更新CollectionDemo3类,演示集合之间的操作:交集,差集,判断子集

--- src/main/java/collection/CollectionDemo3.java
+++ src/main/java/collection/CollectionDemo3.java
@@ -24,10 +24,39 @@ public class CollectionDemo3 {
             将给定集合所有元素添加到当前集合中(并集)
             操作后当前集合发生了改变则返回true
          */
-//        c1.addAll(c2);
-        c2.addAll(c1);
+        c1.addAll(c2);
+//        c2.addAll(c1);
         System.out.println("c1:"+c1);
         System.out.println("c2:"+c2);
+
+
+        Collection c3 = new ArrayList();
+        c3.add("c++");
+        c3.add("android");
+//        c3.add("php");
+        System.out.println("c3:"+c3);
+        /*
+            retainAll(Collection c)
+            取交集,仅保留当前集合中与给定集合的共有元素
+         */
+//        c1.retainAll(c3);
+//        System.out.println("c1:"+c1);
+//        System.out.println("c3:"+c3);
+
+        /*
+            removeAll(Collection c)
+            差集,删除当前集合中与给定集合的共有元素
+         */
+//        c1.removeAll(c3);
+//        System.out.println("c1:"+c1);
+//        System.out.println("c3:"+c3);
+
+        /*
+            boolean containsAll(Collection c)
+            判断给定集合是否为当前集合的子集
+         */
+        boolean containsAll = c1.containsAll(c3);
+        System.out.println("包含所有:"+containsAll);
     }
 }
 
