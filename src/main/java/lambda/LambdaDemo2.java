7bbef07d9b9dd96ba92947fa2cd1f917f3893651
范传奇 <83123095@qq.com>
2024-01-18 11:43:29 +0800

    更新LambdaDemo2类，演示方法引用的写法

--- src/main/java/lambda/LambdaDemo2.java
+++ src/main/java/lambda/LambdaDemo2.java
@@ -21,7 +21,22 @@ public class LambdaDemo2 {
             System.out.println(s);
         }
         //lambda表达式如果只有一个参数时，参数列表的"()"可以忽略不写
-        list.forEach(s->System.out.println(s));
+//        list.forEach(s->System.out.println(s));
+        /*
+            方法引用:
+            当lambda表达式的参数列表和表达式中调用的方法参数列表一致时，可以使用方法引用写法
+            语法:
+            对象::方法     对应成员方法
+            类名::方法     对应静态方法
+
+            例如:
+            list.forEach((s)->System.out.println(s));
+                         ^^^                    ^^^
+                      lambda参数-----一致-------调用方法的参数
+            可以简写为:
+            list.forEach(System.out::println);
+         */
+        list.forEach(System.out::println);
     }
 }
 
