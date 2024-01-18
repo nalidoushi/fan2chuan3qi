bdff9a57b110fefd00560a1c8b472101a5f0b331
范传奇 <83123095@qq.com>
2024-01-18 11:05:36 +0800

    创建LambdaDemo1类，演示lambda表达式

---
+++ src/main/java/lambda/LambdaDemo1.java
@@ -0,0 +1,45 @@
+package lambda;
+
+import java.util.Comparator;
+
+/**
+ * JDK8之后，java推出了一个新特性:lambda表达式
+ *
+ * lambda表达式可以用更简洁的语法来创建匿名内部类
+ * 语法:
+ * (参数列表)->{
+ *     方法体
+ * }
+ *
+ * lambda表达式可以更突出重点逻辑，而不再赘述匿名内部类的语法
+ */
+public class LambdaDemo1 {
+    public static void main(String[] args) {
+        //字符串的比较规则:字符从多到少排序需要
+        Comparator<String> c = new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o2.length()-o1.length();
+            }
+        };
+        //lambda表达式:省去了匿名内部类中接口和方法签名部分。在参数列表和方法体之间加上"->"
+        Comparator<String> c1 = (String o1, String o2)->{
+            return o2.length()-o1.length();
+        };
+
+        //参数类型可以忽略
+        Comparator<String> c2 = (o1, o2)->{
+            return o2.length()-o1.length();
+        };
+
+        //如果方法体只有一句代码，方法体的"{}"可以忽略，同时return关键字也要一同忽略
+        Comparator<String> c3 = (o1, o2)->o2.length()-o1.length();
+
+    }
+}
+
+
+
+
+
+
