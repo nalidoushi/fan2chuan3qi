5574756221da52e896988654a0f42c8464617b7a
范传奇 <83123095@qq.com>
2024-01-19 16:18:27 +0800

    基于IO实现文件的基本复制

--- src/main/java/io/FileCopyDemo.java
+++ src/main/java/io/FileCopyDemo.java
@@ -1,9 +1,26 @@
 package io;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * 文件复制应用案例
+ */
 public class FileCopyDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //1.创建流对象(输入流，输出流)
+        FileInputStream fis=new FileInputStream("a.png");
+        FileOutputStream fos=new FileOutputStream("b.png");
         //2.读写文件数据(复制)
+        int data;
+        while((data=fis.read())!=-1){//当读到文件尾时返回值是-1
+            //将读取的数据写入到文件
+            fos.write(data);
+        }
         //3.释放资源(关闭流)
+        fis.close();
+        fos.close();
     }
 }
