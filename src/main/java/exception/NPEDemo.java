23c86f5ea7e57bf27066d904b9a05eb6a8347728
范传奇 <83123095@qq.com>
2024-01-22 11:11:03 +0800

    try{}catch(..){}语句的基本应用

--- src/main/java/exception/NPEDemo.java
+++ src/main/java/exception/NPEDemo.java
@@ -4,11 +4,25 @@ public class NPEDemo {//NullPointerException
 
 
     public static void main(String[] args) {
-          doConvert(null);
-          System.out.println("main方法结束了");
+        doStringOper02(null);
     }
-    public static String doConvert(String content){
-        System.out.println(content.toLowerCase());
-        return "程序正常结束";
+    public static void doStringOper01(String str){
+        //如下语句可能会出现空指针异常
+        String result=str.toUpperCase();
+        //假如上面的语句出现了异常，下面输出语句是不会执行的。
+        System.out.println(result);
+    }
+    public static void doStringOper02(String str){
+        //假如希望如下语句出现了异常，后面的语句还要继续执行。
+        //此时我们可以使用try{}catch(xxx){}...对可能出现异常的语句进行处理
+        String result;
+        try{
+            result=str.toUpperCase();
+        }catch(NullPointerException e){
+            System.out.println("程序中的str的值不能null");
+            result=null;
+        }
+        //假如上面的语句出现了异常，下面输出语句是不会执行的。
+        System.out.println(result);
     }
 }
