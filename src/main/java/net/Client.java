43f1e578c2e76dafea216a12c4c4dd5a8a104b79
范传奇 <83123095@qq.com>
2024-01-23 10:23:47 +0800

    Socket数据交互

--- src/main/java/net/Client.java
+++ src/main/java/net/Client.java
@@ -1,20 +1,38 @@
 package net;
 
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 
 /**
  * 网络通讯中的客户端
  */
 public class Client {
+    Socket socket;
     public  Client(){
         try {
-            Socket socket = new Socket("127.0.0.1", 8088);
+           socket = new Socket("127.0.0.1", 8088);
         }catch (IOException e){
             e.printStackTrace();
         }
     }
+    public void start(){
+        ObjectOutputStream out=null;
+        try
+        {
+          out=new ObjectOutputStream(socket.getOutputStream());
+          out.writeUTF("hello 8088");
+          out.flush();
+        }catch (IOException e){
+          e.printStackTrace();
+        }finally {
+         //try{socket.close();}catch (Exception e){}
+        }
+    }
     public static void main(String[] args) throws IOException {
-       Client client1=new Client();
+        //与服务器建立连接
+        Client client1=new Client();
+       //与服务器进行数据通讯
+        client1.start();
     }
 }
--- src/main/java/net/Server.java
+++ src/main/java/net/Server.java
@@ -1,11 +1,13 @@
 package net;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 /**
- * 网络通讯中服务端
+ * 网络通讯中服务端,网络通讯时,先要启动服务器
  */
 public class Server {
     ServerSocket server;
@@ -23,6 +25,17 @@ public class Server {
             //接受客户端的连接
             Socket client = server.accept();//accept是一个阻塞式方法
             System.out.println("有客户端连接到服务器了");
+            ObjectInputStream in=null;
+            try
+            {
+                in=new ObjectInputStream(client.getInputStream());
+                String content=in.readUTF();
+                System.out.println(content);
+            }catch (IOException e){
+                e.printStackTrace();
+            }finally {
+                //try{client.close();}catch (Exception e){}
+            }
         }catch (IOException e){
             e.printStackTrace();
         }
@@ -30,6 +43,5 @@ public class Server {
     public static void main(String[] args) throws IOException {
            Server server=new Server(8088);
            server.start();
-
     }
 }
