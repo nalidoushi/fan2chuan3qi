6abf8ded634de5b64e0c4d29732e738879e4ba26
范传奇 <83123095@qq.com>
2024-02-18 17:00:01 +0800

    SQL

--- src/main/java/DDD/Demo02.java
+++ src/main/java/DDD/Demo02.java
@@ -1,17 +1,20 @@
 package DDD;
 
+class DivZeroException extends RuntimeException{
+}
+
 public class Demo02 {
     public static void main(String[] args) {
         try {
             div(3,0);
-        } catch (OutOfMemoryError e) {
-            System.out.println("捕获到了~~·");
+        } catch (DivZeroException e) {
+            e.printStackTrace();
         }
     }
-    public static int div(int a,int b){
+
+    public static int div(int a,int b) {
         if(b == 0){
-            //????
-            throw new OutOfMemoryError();
+            throw new DivZeroException();
         }
         return a/b;
     }
